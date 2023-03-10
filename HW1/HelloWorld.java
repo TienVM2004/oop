0905082eecbc3f2606a2e849cca37d0c05948f33
ineedsumsnack <vuminhtien2004@gmail.com>
2023-03-11 06:09:50 +0700

    first commit

---
+++ HW1/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Vũ Minh Tiến    22022645    AI1    ineedsumsnack    22022645@vnu.edu.vn");
+        for(int i=9;i>=1;i--){
+            System.out.println(i+ " bottles of beer on the wall, "+i+" bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+
+}
