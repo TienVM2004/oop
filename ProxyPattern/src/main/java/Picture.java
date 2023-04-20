f831a187d674d64fc8641967b8215b3ac362e6dd
ineedsumsnack <vuminhtien2004@gmail.com>
2023-04-20 08:25:49 +0700

    add ProxyPattern

--- ProxyPattern/src/main/java/Picture.java
+++ ProxyPattern/src/main/java/Picture.java
@@ -1,14 +1,28 @@
 import java.awt.*;
 
 public class Picture extends Graphic {
+    private String fileName;
+    private Dimension extent;
+    private Point position;
+    private Picture picture;
+
+
+    Picture(String name, Dimension ext, Point point) {
+        fileName = name;
+        extent = ext;
+        position = point;
+    }
     public void draw (Point position) {
 
     }
+    public String getFileName(){
+        return fileName;
+    }
     public Dimension getExtent(){
-        return new Dimension();
+        return extent;
     }
     public Point getPosition(){
-        return new Point();
+        return position;
     }
     public void handleMouse (Event mouseEvent){
 
@@ -18,6 +32,17 @@ public class Picture extends Graphic {
     }
     public void load(){
 
+    }
+    public boolean equals(Object obj){
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Picture)) {
+            return false;
+        }
+        Picture other = (Picture) obj;
+        if(other.getExtent().equals(this.extent) && other.getPosition().equals(this.position) && other.getFileName().equals(this.fileName)) return true;
+        else return false;
     }
 
 }
